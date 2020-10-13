0e7211c3269ed3d9e975ceb7c7ed800c1106f87c
Hellboy-Aaryan <72555917+Hellboy-Aaryan@users.noreply.github.com>
2020-10-13 11:00:09 +0530

    Add files via upload

---
+++ Raganrok/plugins/gaali.py
@@ -0,0 +1,23 @@
+"""COMMAND : .gaali"""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd("gaali"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "`Thari maka lawda behnke lawde thari makai chut me mera masalaThari maka lawda behnke lawde thari makai chut me mera masalaThari maka lawda behnke lawde thari makai chut me mera masalaThari maka lawda behnke lawde thari makai chut me mera masalaThari maka lawda behnke lawde thari makai chut me mera masalaThari maka lawda behnke lawde thari makai chut me mera masalaThari maka lawda behnke lawde thari makai chut me mera masalaThari maka lawda behnke lawde thari makai chut me mera masala `"
+    mentions =  "`🤣🤣🤣𝐌𝐀𝐓𝐇𝐀𝐑𝐂𝐇𝐎𝐎𝐎𝐎𝐎𝐃 𝐁𝐇𝐄𝐍 𝐊𝐄 𝐋𝐎𝐃𝐄𝐄 𝐁𝐇𝐄𝐍 𝐊𝐄 𝐋𝐔𝐔𝐔𝐔𝐔𝐔𝐍𝐃  𝐀𝐏𝐍𝐈 𝐌𝐀𝐀 𝐁𝐔𝐋𝐀 𝐋𝐄..... 𝐂𝐇𝐎𝐎𝐃𝐍𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈 𝐇 𝐌𝐄𝐑𝐄𝐊𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎 𝐂𝐇𝐔𝐓𝐓𝐓𝐓𝐓 𝐌 𝐋𝐀𝐍𝐃𝐃𝐃𝐃𝐃𝐃𝐃𝐃𝐃𝐃 𝐏𝐃𝐄𝐆𝐀 𝐓𝐎.......... 𝐊𝐇𝐔𝐍 𝐇𝐈 𝐊𝐇𝐔𝐍𝐍𝐍𝐍.......𝐌𝐄𝐑𝐈 𝐉𝐀𝐀𝐍𝐄𝐌𝐀𝐍𝐍𝐍𝐍𝐍 𝐊𝐎 𝐓𝐘𝐀𝐀𝐑 𝐑𝐊𝐇𝐍𝐀 𝐒𝐄𝐗𝐘𝐘𝐘𝐘𝐘𝐘 𝐊𝐀𝐏𝐃𝐎𝐎𝐎𝐎𝐎𝐎 𝐌𝐌𝐌𝐌𝐌 ....... 𝐁𝐄𝐃.     𝐓𝐔𝐓𝐄𝐆𝐀𝐀𝐀𝐀𝐀𝐀𝐀 💥💥💥💥💦💦💦💦💦💦💋💋💋💦💋🤣💋💦🤣🤣💦💋💋💋💦🤣🤣🤣🤣🤣🤣💦💦💦🤣🤣🤣🤣💦💋🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣𝐂𝐋𝐈𝐂𝐊 XNXX.COM 𝐓𝐄𝐑𝐈 𝐌𝐀 𝐂𝐇𝐎𝐃𝐈 𝐀𝐍𝐃𝐀𝐑𝐓𝐀𝐊💦💦💦💦💦💦💋💋💋💋💋💋💋💋💋💋💋🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣 @sarswatichandra  🤣🤣🤣𝐌𝐀𝐓𝐇𝐀𝐑𝐂𝐇𝐎𝐎𝐎𝐎𝐎𝐃 𝐁𝐇𝐄𝐍 𝐊𝐄 𝐋𝐎𝐃𝐄𝐄 𝐁𝐇𝐄𝐍 𝐊𝐄 𝐋𝐔𝐔𝐔𝐔𝐔𝐔𝐍𝐃  𝐀𝐏𝐍𝐈 𝐌𝐀𝐀 𝐁𝐔𝐋𝐀 𝐋𝐄..... 𝐂𝐇𝐎𝐎𝐃𝐍𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈𝐈 𝐇 𝐌𝐄𝐑𝐄𝐊𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐎 𝐂𝐇𝐔𝐓𝐓𝐓𝐓𝐓 𝐌 𝐋𝐀𝐍𝐃𝐃𝐃𝐃𝐃𝐃𝐃𝐃𝐃𝐃 𝐏𝐃𝐄𝐆𝐀 𝐓𝐎.......... 𝐊𝐇𝐔𝐍 𝐇𝐈 𝐊𝐇𝐔𝐍𝐍𝐍𝐍.......𝐌𝐄𝐑𝐈 𝐉𝐀𝐀𝐍𝐄𝐌𝐀𝐍𝐍𝐍𝐍𝐍 𝐊𝐎 𝐓𝐘𝐀𝐀𝐑 𝐑𝐊𝐇𝐍𝐀 𝐒𝐄𝐗𝐘𝐘𝐘𝐘𝐘𝐘 𝐊𝐀𝐏𝐃𝐎𝐎𝐎𝐎𝐎𝐎 𝐌𝐌𝐌𝐌𝐌 ....... 𝐁𝐄𝐃.     𝐓𝐔𝐓𝐄𝐆𝐀𝐀𝐀𝐀𝐀𝐀𝐀 💥💥💥💥💦💦💦💦💦💦💋💋💋💦💋🤣💋💦🤣🤣💦💋💋💋💦🤣🤣🤣🤣🤣🤣💦💦💦🤣🤣🤣🤣💦💋🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣𝐂𝐋𝐈𝐂𝐊 XNXX.COM 𝐓𝐄𝐑𝐈 𝐌𝐀 𝐂𝐇𝐎𝐃𝐈 𝐀𝐍𝐃𝐀𝐑𝐓𝐀𝐊💦💦💦💦💦💦💋💋💋💋💋💋💋💋💋💋💋🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣`"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
