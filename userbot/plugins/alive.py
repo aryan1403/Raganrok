de30ba447293eaf5474db125fec956e828dea646
Hellboy-Aaryan <72555917+Hellboy-Aaryan@users.noreply.github.com>
2020-10-15 16:12:47 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,17 +7,29 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Raganork User"
 PM_IMG = "https://telegra.ph/file/0e00f9e103c4a21ed35a5.jpg"
 pm_caption = "🔱 **Ɽǟɢǟռօʀӄɮð† Is Online** 🔱\n\n"
 
-pm_caption += f"🔸🔹 **ᎷᎽ ᏰᏫᏕᏕ**           :   {DEFAULTUSER}\n"
+pm_caption += f"🔸🔹 **ᎷᎽ ℓσя∂**           :   {DEFAULTUSER}\n"
 
-pm_caption += "🔹🔸 тєℓєтнσи νєяѕισи   :   1.15.0 \n"
+pm_caption += "🔹🔸𝙎𝙮𝙨𝙩𝙚𝙢 𝙎𝙩𝙖𝙩𝙨\n"
 
-pm_caption += "🔸🔹 σffι¢ιαℓ ¢нαииєℓ   :   [ᴊᴏɪɴ](https://t.me/Raganork_Official)\n"
+pm_caption += "🔸🔹Os     :Kali Linux 😈😈 \n"
 
-pm_caption += "🔹🔸 σffι¢ιαℓ gяσυρ     :   [ᴊᴏɪɴ](https://t.me/Raganork_bot_chat)\n"
+pm_caption += "🔹🔸 тєℓєтнσи νєяѕισи   :   1.15.1 \n"
 
-pm_caption += "🔸🔹 ℓι¢єиѕє            :   [ӀíϲҽղՏҽ](https://github.com/HellBoy-Aaryan/Raganork/blob/master/LICENSE)\n"
+pm_caption += "🔸🔹 Ɽǟɢǟռօʀӄɮð† νєяѕισи    : 2.0 \n"
 
-pm_caption += "🔹🔸 ¢σρуяιgнт          :   [Raganork-Owner](https://github.com/HellBoy-Aaryan)\n"
+pm_caption += "🔹🔸ρунтσи    :3.8.5\n"
+
+pm_caption += "🔸🔹\n"
+
+pm_caption += "🔹🔸 σffι¢ιαℓ ¢нαииєℓ   :   [ᴊᴏɪɴ](https://t.me/Raganork_Official)\n"
+
+pm_caption += "🔸🔹 σffι¢ιαℓ gяσυρ     :   [ᴊᴏɪɴ](https://t.me/Raganork_bot_chat)\n"
+
+pm_caption += "🔹🔸 ℓι¢єиѕє            :   [ӀíϲҽղՏҽ](https://github.com/HellBoy-Aaryan/Raganork/blob/master/LICENSE)\n"
+
+pm_caption += "🔸🔹 ¢σρуяιgнт          :   [Raganork-Owner](https://github.com/HellBoy-Aaryan)\n"
+
+pm_caption += "🔹🔸 Database Functionality          :   All Set 👌 \n"
 
 pm_caption += " [...▄███▄███▄\n....█████████\n.......▀█████▀\n............▀█▀\n](https://t.me/Raganork_Official)\n"
 #@command(outgoing=True, pattern="^.alive$")
