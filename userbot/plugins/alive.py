56c9eef7ca418ff90af9aa5fcabbe885b858a642
Hellboy-Aaryan <72555917+Hellboy-Aaryan@users.noreply.github.com>
2020-10-17 17:30:37 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,23 +5,23 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Raganork User"
 PM_IMG = "https://telegra.ph/file/0e00f9e103c4a21ed35a5.jpg"
-pm_caption = "🔥🔥**Ɽǟɢǟռօʀӄɮð† IS ONLINE🔥🔥\n\n\n"
+pm_caption = "🔥🔥**Ɽǟɢǟռօʀӄɮð† IS ONLINE🔥🔥\n\n"
 
-pm_caption += f"⚔️⚔️**му ℓσя∂**⚔️⚔️       : {DEFAULTUSER}\n\n"
+pm_caption += f"⚔️⚔️**му ℓσя∂**⚔️⚔️: {DEFAULTUSER}\n"
 
-pm_caption += "⚡️⚡️**тєℓєтнσи**⚡️⚡️   : 1.15.0 \n\n"
+pm_caption += "⚡️⚡️**тєℓєтнσи**⚡️⚡️: 1.15.0 \n"
 
-pm_caption += "⚡️⚡️**Os[Linux]**⚡️⚡️   : ʟɪɴᴜx ᴍᴀɴᴅʀɪᴠᴀ \n\n"
+pm_caption += "⚡️⚡️**Os[Linux]**⚡️⚡️: ʟɪɴᴜx ᴍᴀɴᴅʀɪᴠᴀ \n"
 
-pm_caption += "😈😈**Ɽǟɢǟռօʀӄɮð†**😈😈         : 2.0\n\n"
+pm_caption += "😈😈**Ɽǟɢǟռօʀӄɮð†**😈😈: 2.0\n"
 
-pm_caption += "✨✨**¢нαииєℓ**✨✨     : [ᴊᴏɪɴ](https://t.me/HellBot_Official)\n\n"
+pm_caption += "✨✨**¢нαииєℓ**✨✨: [ᴊᴏɪɴ](https://t.me/HellBot_Official)\n"
 
-pm_caption += "⚜️⚜️**gяσυρ**⚜️⚜️.         : [ᴊᴏɪɴ](https://t.me/HellBot_Official_Chat)\n\n"
+pm_caption += "⚜️⚜️**gяσυρ**⚜️⚜️: [ᴊᴏɪɴ](https://t.me/HellBot_Official_Chat)\n"
 
-pm_caption += "💫💫**ᴄᴜʀʀᴇɴᴛ ᴅɪʀᴇᴄᴛᴏʀʏ**💫💫.         : ᴋᴀʟɪ_ᴘᴀʀʀᴏᴛ_ᴛᴇʀᴍɪɴᴀʟ\n\n"
+pm_caption += "💫💫**ᴄᴜʀʀᴇɴᴛ ᴅɪʀᴇᴄᴛᴏʀʏ**💫💫: ᴋᴀʟɪ_ᴘᴀʀʀᴏᴛ_ᴛᴇʀᴍɪɴᴀʟ\n"
 
-pm_caption += "🔥🔥**ᴅᴇᴠᴇʟᴏᴘᴇʀ 🔥🔥      : [Raganork-Owner](https://t.me/HELLBOY_OP)\n\n"
+pm_caption += "🔥🔥**ᴅᴇᴠᴇʟᴏᴘᴇʀ 🔥🔥: [Raganork-Owner](https://t.me/HELLBOY_OP)\n"
 
 
 pm_caption += " [...▄███▄███▄\n....█████████\n.......▀█████▀\n............▀█▀\n](https://t.me/HELLBOY_OP)\n"
