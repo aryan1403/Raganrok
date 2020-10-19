70d91c5e3b39f32a8526a27036731626f596b9bb
Hellboy-Aaryan <72555917+Hellboy-Aaryan@users.noreply.github.com>
2020-10-19 12:02:30 +0530

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -28,10 +28,10 @@ async def install(event):
                 path1 = Path(downloaded_file_name)
                 shortname = path1.stem
                 load_module(shortname.replace(".py", ""))
-                await event.edit("Installed Plugin `{}` By @HellBot_Official".format(os.path.basename(downloaded_file_name)))
+                await event.edit("Installed Plugin `{}` By @Raganork_Official".format(os.path.basename(downloaded_file_name)))
             else:
                 os.remove(downloaded_file_name)
-                await event.edit("Errors! This plugin is already installed/pre-installed.")
+                await event.edit("Errors! This plugin is already installed.")
         except Exception as e:  # pylint:disable=C0103,W0703
             await event.edit(str(e))
             os.remove(downloaded_file_name)
