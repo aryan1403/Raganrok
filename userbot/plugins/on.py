fe3055c6b5f223e23b59c0d979e9582ccad16042
Hellboy-Aaryan <72555917+Hellboy-Aaryan@users.noreply.github.com>
2020-10-28 11:43:30 +0530

    Update on.py

--- userbot/plugins/on.py
+++ userbot/plugins/on.py
@@ -4,12 +4,12 @@ from userbot.utils import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Raganork user"
-PM_IMG = "https://telegra.ph/file/02d3b3c776165c0f7ffbf.jpg"
+PM_IMG = "https://telegra.ph/file/a4ef33e7ce73ae9558330.jpg"
 pm_caption = "This is **Raganork userbot !**\n\n"
 
 pm_caption += f"✨Servant of my master \n  : {DEFAULTUSER} \n"
 
-pm_caption += "✨**My Os**   ☞ ʟɪɴᴜx ᴍᴀɴᴅʀɪᴠᴀ  \n"
+pm_caption += "✨**My Os**   : ʟɪɴᴜx ᴍᴀɴᴅʀɪᴠᴀ  \n"
 
 pm_caption += "**✨Mandriva Version**  : 1.07.3 \n"
 
