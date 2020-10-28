ca917f83d67425a03831ba172e1e38024afbcab0
Sipak <68851382+sipakisking@users.noreply.github.com>
2020-10-28 10:49:51 +0530

    .
    Ye nhi chalega kyun dekh rhe ho BC

--- userbot/plugins/on.py
+++ userbot/plugins/on.py
@@ -1,23 +1,77 @@
 import asyncio
+
+
+
+import random
+
+
+
 from telethon import events
+
+
+
 from userbot.utils import admin_cmd
+
+
+
 from userbot import ALIVE_NAME
+
+
+
 from telethon.tl.types import ChannelParticipantsAdmins
+
+
+
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Raganork user"
-PM_IMG = "https://telegra.ph/file/a4ef33e7ce73ae9558330.jpg"
+
+
+
+#Social_distancing
+
+
+
+IMGS = ["http://telegra.ph/file/a4ef33e7ce73ae9558330.jpg", "http://telegra.ph/file/fbf8619abb11516420145.jpg", "http://telegra.ph/file/efadde4adf9674be0ea38.jpg", "http://telegra.ph/file/353837fb6dad1a7aa44c0.jpg"]
+    
+    
+    
+
+    
+PM_IMG = random.choice(IMGS)
+
+
+
+
+#Social_distancing
+
+
+
 pm_caption = "This is **Raganork userbot !**\n\n"
+
 
 pm_caption += f"✨Servant of my master \n  : {DEFAULTUSER} \n"
 
+
 pm_caption += "✨**My Os**   ☞ ʟɪɴᴜx ᴍᴀɴᴅʀɪᴠᴀ  \n"
+
 
 pm_caption += "**✨Mandriva Version**  : 1.07.3 \n"
 
+
 pm_caption += "✨**Support Channel**   : [ᴊᴏɪɴ](https://t.me/Raganork_Official)\n"
+
 
 pm_caption += "✨**Support group** : [ᴊᴏɪɴ](https://t.me/Raganork_bot_chat)\n"
 
+
 pm_caption += "✨**Developer** :  [@HELLBOY_OP](https://t.me/Hellboy_Pikachu)  \n\n"
+
+
+
+
+#Social_distancing
+
+
+
 
 @borg.on(admin_cmd(pattern=r"on"))
 async def amireallyalive(yes):
